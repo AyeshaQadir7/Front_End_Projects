18d5037779e989e17e975ab9df00f1aef4783bfd
Ayesha A.Qadir <ayeshaabdulqadir07@gmail.com>
2024-06-06 14:27:55 +0500

    Front_End_Project

---
+++ Tourest_Website/script.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/** navbar toggle **/
+
+const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
+const header = document.querySelector("[data-header]");
+
+navToggleBtn.addEventListener("click", function () {
+  this.classList.toggle("active");
+  header.classList.toggle("active");
+});
+
+// // show go to top btn when scroll window to 500px
+
+const goTopBtn = document.querySelector("[data-go-top]")
+
+window.addEventListener("scroll", function () {
+  this.window.scrollY > 500 ? goTopBtn.classList.add("active") : goTopBtn.classList.remove("active");
+});
+
